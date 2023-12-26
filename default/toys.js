c371703230c8d6443cc8e12cb460ef2ee88c7a75
Michael Lulev <miki1990@gmail.com>
2023-12-27 01:32:30 +0200

    Add labels to toys

--- default/toys.js
+++ default/toys.js
@@ -1,86 +1,165 @@
+import { utilService } from "../services/util.service.js"
+
+
 export const defaultToys = [
     {
-        _id: "toy001",
+        _id: utilService.makeId(),
         name: "Plush Teddy Bear",
         description: "A soft, cuddly teddy bear in pastel colors, perfect for hugs and comfort.",
         price: 15.99,
-        stock: 20
+        stock: 20,
+        labels: ["Plush", "Teddy Bear", "Soft", "Cuddly"]
     },
     {
-        _id: "toy002",
+        _id: utilService.makeId(),
         name: "Colorful Wooden Puzzle",
         description: "A bright, educational wooden puzzle, ideal for developing motor skills.",
         price: 12.99,
-        stock: 0  // Out of stock
+        stock: 5,
+        labels: ["Educational", "Wooden", "Puzzle", "Colorful"]
     },
     {
-        _id: "toy003",
+        _id: utilService.makeId(),
         name: "Superhero Action Figure",
         description: "A dynamic superhero figure, ready for adventurous play and storytelling.",
         price: 18.99,
-        stock: 25
+        stock: 0,
+        labels: ["Action Figure", "Superhero", "Dynamic", "Collectible"]
     },
     {
-        _id: "toy004",
+        _id: utilService.makeId(),
         name: "Family Board Game",
         description: "A classic board game that promises fun and bonding for the whole family.",
         price: 20.99,
-        stock: 30
+        stock: 30,
+        labels: ["Board Game", "Family", "Strategy", "Interactive"]
     },
     {
-        _id: "toy005",
+        _id: utilService.makeId(),
         name: "Remote-Controlled Car",
         description: "A high-speed remote-controlled car for thrilling races and play.",
         price: 29.99,
-        stock: 10
+        stock: 10,
+        labels: ["Remote-Controlled", "Car", "Electronic", "Speed"]
     },
     {
-        _id: "toy006",
+        _id: utilService.makeId(),
         name: "Detailed Dollhouse",
         description: "An intricately designed dollhouse, complete with miniature furniture.",
         price: 45.99,
-        stock: 0  // Out of stock
+        stock: 2,
+        labels: ["Dollhouse", "Miniature", "Intricate", "Imaginative Play"]
     },
     {
-        _id: "toy007",
+        _id: utilService.makeId(),
         name: "Kid's Musical Instruments",
         description: "Colorful and fun musical instruments, perfect for introducing kids to music.",
         price: 22.99,
-        stock: 18
+        stock: 3,
+        labels: ["Musical", "Instruments", "Educational", "Colorful"]
     },
     {
-        _id: "toy008",
+        _id: utilService.makeId(),
         name: "Outdoor Swing Set",
         description: "A sturdy and safe swing set, ideal for hours of outdoor fun.",
         price: 120.99,
-        stock: 5
+        stock: 1,
+        labels: ["Outdoor", "Swing Set", "Playground", "Active Play"]
     },
     {
-        _id: "toy009",
+        _id: utilService.makeId(),
         name: "Vibrant Spinner Yo-Yo",
         description: "A colorful, patterned yo-yo, perfect for fun and skillful play.",
         price: 9.99,
-        stock: 40
+        stock: 0,
+        labels: ["Yo-Yo", "Skill Toy", "Vibrant", "Hand-Eye Coordination"]
     },
     {
-        _id: "toy010",
+        _id: utilService.makeId(),
         name: "Classic Wooden Express Train",
         description: "Timeless wooden train toy, combining durability with classic charm.",
         price: 24.99,
-        stock: 22
+        stock: 5,
+        labels: ["Wooden Train", "Classic Toy", "Collectible", "Imaginative"]
     },
     {
-        _id: "toy011",
+        _id: utilService.makeId(),
         name: "Sunny Quackers Rubber Duck",
         description: "Bright and playful rubber duck for joyful bath times.",
         price: 5.99,
-        stock: 35
+        stock: 7,
+        labels: ["Rubber Duck", "Bath Toy", "Water Play", "Colorful"]
     },
     {
-        _id: "toy012",
+        _id: utilService.makeId(),
         name: "Skyward Precision Model Airplane",
         description: "Intricately detailed model airplane for enthusiasts and collectors.",
         price: 39.99,
-        stock: 0  // Out of stock
+        stock: 0,
+        labels: ["Model Airplane", "Detailed", "Collectible", "Aviation"]
+    },
+    {
+        _id: utilService.makeId(),
+        name: "Whimsical Unicorn Plush Toy",
+        description: "A vibrant and soft unicorn plush, perfect for fantasy-loving kids.",
+        price: 17.99,
+        stock: 15,
+        labels: ["Unicorn", "Plush Toy", "Soft", "Fantasy"]
+    },
+    {
+        _id: utilService.makeId(),
+        name: "Intricate Puzzle Cube",
+        description: "A colorful, challenging puzzle cube for brain-teasing fun.",
+        price: 11.99,
+        stock: 0,
+        labels: ["Puzzle Cube", "Brain Teaser", "Colorful", "Challenging"]
+    },
+    {
+        _id: utilService.makeId(),
+        name: "Fluffy Panda Pillow",
+        description: "A soft, fluffy panda-shaped pillow, great for cuddling.",
+        price: 14.99,
+        stock: 10,
+        labels: ["Panda Pillow", "Soft", "Cuddly", "Animal-Shaped"]
+    },
+    {
+        _id: utilService.makeId(),
+        name: "Electronic Toy Train",
+        description: "A modern electronic train with vibrant colors and realistic details.",
+        price: 29.99,
+        stock: 5,
+        labels: ["Electronic Train", "Realistic", "Modern", "Toy Vehicle"]
+    },
+    {
+        _id: utilService.makeId(),
+        name: "Futuristic Toy Robot",
+        description: "A brightly colored toy robot with a modern and playful design.",
+        price: 21.99,
+        stock: 12,
+        labels: ["Electronic", "Robotics", "Interactive", "Futuristic"]
+    },
+    {
+        _id: utilService.makeId(),
+        name: "Magical Fairy Doll",
+        description: "A fairy doll with delicate wings and enchanting details, perfect for fantasy play.",
+        price: 16.99,
+        stock: 14,
+        labels: ["Fantasy", "Fairy", "Imaginative Play", "Dolls"]
+    },
+    {
+        _id: utilService.makeId(),
+        name: "Colorful Kite",
+        description: "A vibrant kite soaring in the sky, symbolizing joy and outdoor play.",
+        price: 13.99,
+        stock: 18,
+        labels: ["Outdoor", "Flight", "Colorful", "Wind Play"]
+    },
+    {
+        _id: utilService.makeId(),
+        name: "Miniature Toy Car",
+        description: "A miniature toy car with realistic details and a sleek design.",
+        price: 7.99,
+        stock: 20,
+        labels: ["Miniature", "Vehicle", "Collectible", "Realistic"]
     }
 ]
