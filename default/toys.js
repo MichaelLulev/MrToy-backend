d904fe5c08f58c75b1d26e0d9e135f948f61fb25
Michael Lulev <miki1990@gmail.com>
2023-12-27 16:32:21 +0200

    Change toy labels

--- default/toys.js
+++ default/toys.js
@@ -13,7 +13,7 @@ const defaultToys = [
         description: "A soft, cuddly teddy bear in pastel colors, perfect for hugs and comfort.",
         price: 15.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Plush", "Teddy Bear", "Soft", "Cuddly"]
+        labels: ["Plush", "Soft"]
     },
     {
         _id: utilService.makeId(),
@@ -21,7 +21,7 @@ const defaultToys = [
         description: "A bright, educational wooden puzzle, ideal for developing motor skills.",
         price: 12.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Educational", "Wooden", "Puzzle", "Colorful"]
+        labels: ["Educational", "Puzzle"]
     },
     {
         _id: utilService.makeId(),
@@ -29,7 +29,7 @@ const defaultToys = [
         description: "A dynamic superhero figure, ready for adventurous play and storytelling.",
         price: 18.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Action Figure", "Superhero", "Dynamic", "Collectible"]
+        labels: ["Action", "Collectible"]
     },
     {
         _id: utilService.makeId(),
@@ -37,7 +37,7 @@ const defaultToys = [
         description: "A classic board game that promises fun and bonding for the whole family.",
         price: 20.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Board Game", "Family", "Strategy", "Interactive"]
+        labels: ["Family", "Game"]
     },
     {
         _id: utilService.makeId(),
@@ -45,7 +45,7 @@ const defaultToys = [
         description: "A high-speed remote-controlled car for thrilling races and play.",
         price: 29.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Remote-Controlled", "Car", "Electronic", "Speed"]
+        labels: ["Electronic", "Vehicle"]
     },
     {
         _id: utilService.makeId(),
@@ -53,7 +53,7 @@ const defaultToys = [
         description: "An intricately designed dollhouse, complete with miniature furniture.",
         price: 45.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Dollhouse", "Miniature", "Intricate", "Imaginative Play"]
+        labels: ["Imaginative Play", "Collectible"]
     },
     {
         _id: utilService.makeId(),
@@ -61,7 +61,7 @@ const defaultToys = [
         description: "Colorful and fun musical instruments, perfect for introducing kids to music.",
         price: 22.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Musical", "Instruments", "Educational", "Colorful"]
+        labels: ["Educational", "Music"]
     },
     {
         _id: utilService.makeId(),
@@ -69,7 +69,7 @@ const defaultToys = [
         description: "A sturdy and safe swing set, ideal for hours of outdoor fun.",
         price: 120.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Outdoor", "Swing Set", "Playground", "Active Play"]
+        labels: ["Outdoor", "Active Play"]
     },
     {
         _id: utilService.makeId(),
@@ -77,7 +77,7 @@ const defaultToys = [
         description: "A colorful, patterned yo-yo, perfect for fun and skillful play.",
         price: 9.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Yo-Yo", "Skill Toy", "Vibrant", "Hand-Eye Coordination"]
+        labels: ["Skill Toy", "Active Play"]
     },
     {
         _id: utilService.makeId(),
@@ -85,7 +85,7 @@ const defaultToys = [
         description: "Timeless wooden train toy, combining durability with classic charm.",
         price: 24.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Wooden Train", "Classic Toy", "Collectible", "Imaginative"]
+        labels: ["Imaginative Play", "Collectible"]
     },
     {
         _id: utilService.makeId(),
@@ -93,7 +93,7 @@ const defaultToys = [
         description: "Bright and playful rubber duck for joyful bath times.",
         price: 5.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Rubber Duck", "Bath Toy", "Water Play", "Colorful"]
+        labels: ["Bath Toy", "Water Play"]
     },
     {
         _id: utilService.makeId(),
@@ -101,7 +101,7 @@ const defaultToys = [
         description: "Intricately detailed model airplane for enthusiasts and collectors.",
         price: 39.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Model Airplane", "Detailed", "Collectible", "Aviation"]
+        labels: ["Collectible", "Aviation"]
     },
     {
         _id: utilService.makeId(),
@@ -109,7 +109,7 @@ const defaultToys = [
         description: "A vibrant and soft unicorn plush, perfect for fantasy-loving kids.",
         price: 17.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Unicorn", "Plush Toy", "Soft", "Fantasy"]
+        labels: ["Plush", "Fantasy"]
     },
     {
         _id: utilService.makeId(),
@@ -117,7 +117,7 @@ const defaultToys = [
         description: "A colorful, challenging puzzle cube for brain-teasing fun.",
         price: 11.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Puzzle Cube", "Brain Teaser", "Colorful", "Challenging"]
+        labels: ["Puzzle", "Brain Game"]
     },
     {
         _id: utilService.makeId(),
@@ -125,7 +125,7 @@ const defaultToys = [
         description: "A soft, fluffy panda-shaped pillow, great for cuddling.",
         price: 14.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Panda Pillow", "Soft", "Cuddly", "Animal-Shaped"]
+        labels: ["Plush", "Soft"]
     },
     {
         _id: utilService.makeId(),
@@ -133,7 +133,7 @@ const defaultToys = [
         description: "A modern electronic train with vibrant colors and realistic details.",
         price: 29.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Electronic Train", "Realistic", "Modern", "Toy Vehicle"]
+        labels: ["Electronic", "Vehicle"]
     },
     {
         _id: utilService.makeId(),
@@ -141,7 +141,7 @@ const defaultToys = [
         description: "A brightly colored toy robot with a modern and playful design.",
         price: 21.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Electronic", "Robotics", "Interactive", "Futuristic"]
+        labels: ["Electronic", "Interactive"]
     },
     {
         _id: utilService.makeId(),
@@ -149,7 +149,7 @@ const defaultToys = [
         description: "A fairy doll with delicate wings and enchanting details, perfect for fantasy play.",
         price: 16.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Fantasy", "Fairy", "Imaginative Play", "Dolls"]
+        labels: ["Fantasy", "Doll"]
     },
     {
         _id: utilService.makeId(),
@@ -157,7 +157,7 @@ const defaultToys = [
         description: "A vibrant kite soaring in the sky, symbolizing joy and outdoor play.",
         price: 13.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Outdoor", "Flight", "Colorful", "Wind Play"]
+        labels: ["Outdoor", "Flight"]
     },
     {
         _id: utilService.makeId(),
@@ -165,6 +165,6 @@ const defaultToys = [
         description: "A miniature toy car with realistic details and a sleek design.",
         price: 7.99,
         stock: Math.max(utilService.getRandInt(16, -4), 0),
-        labels: ["Miniature", "Vehicle", "Collectible", "Realistic"]
+        labels: ["Vehicle", "Collectible"]
     }
 ]
