924e3897bee2a13eb1d5e509a1b64093067a0046
Michael Lulev <miki1990@gmail.com>
2023-12-29 16:31:16 +0200

    Add MUI Joy Buttons and Links

--- server.js
+++ server.js
@@ -119,7 +119,6 @@ app.delete(BASE_URL_TOY_API + '/:toyId', (req, res) => {
 
 // Toy list
 app.get(BASE_URL_TOY_API, (req, res) => {
-    console.log(req.query)
     const filterBy = {
         text: req.query.text,
         stock: req.query.stock,
