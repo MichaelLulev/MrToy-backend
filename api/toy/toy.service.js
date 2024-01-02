8005a77f57b47844c04c6c7d53c82bf373fbff8e
Michael Lulev <miki1990@gmail.com>
2024-01-02 15:22:00 +0200

    Add MongoDB toy service

--- api/toy/toy.service.js
+++ api/toy/toy.service.js
@@ -1,6 +1,5 @@
 import { ObjectId } from 'mongodb'
 
-import { utilService } from '../../services/util.service.js'
 import { loggerService } from '../../services/logger.service.js'
 import { dbService } from '../../services/db.service.js'
 import { getDefaultToys } from '../../default/toys.js'
@@ -16,7 +15,12 @@ export const toyService = {
 
 
 async function query(filterBy, sortBy, pageInfo) {
-    const toyCollection = await dbService.getCollection('toy')
+    try {
+        var toyCollection = await dbService.getCollection('toy')
+    } catch (err) {
+        loggerService.error('Cannot get toy collection: ', err)
+        throw err
+    }
     if (await toyCollection.countDocuments() === 0) {
         loggerService.info('No toys: Creating toys')
         try {
@@ -29,11 +33,10 @@ async function query(filterBy, sortBy, pageInfo) {
         const toys = await toyCollection
             .find(getFilterCriteria(filterBy))
             .sort(getSortCriteria(sortBy))
-            .limit(getPageCriteria(pageInfo))
             .toArray()
-        return toys
+        return getPage(toys, pageInfo)
     } catch (err) {
-        loggerService.error('Cannot find toys: ', err)
+        loggerService.error('Cannot get toys: ', err)
         throw err
     }
 }
@@ -43,90 +46,125 @@ function getFilterCriteria(filterBy) {
     const filterCriteria = {}
     if (filterBy.stock === 'yes') filterCriteria.stock = { $ne: 0 }
     else if (filterBy.stock === 'no' ) filterCriteria.stock = 0
-    const textRegex =                { $regex: filterBy.text, $options: 'i' }
+    const textRegex = { $regex: filterBy.text || '', $options: 'i' }
     filterCriteria.$or = [{ name: textRegex }, { description: textRegex }]
-    filterCriteria.labels = { $in: filterBy.labels }
+    if (filterBy.labels?.length) filterCriteria.labels = { $in: filterBy.labels || [] }
     return filterCriteria
 }
 
-function sort(toys, sortBy) {
-    if (! sortBy) return toys
+function getSortCriteria(sortBy) {
+    if (! sortBy) return {}
     const { field, isAscending } = sortBy
-    const dirMult = isAscending ? 1 : -1
-    return toys.sort((toy1, toy2) => {
-        switch (field) {
-            case 'name':
-            case 'description': return toy1[field].localeCompare(toy2[field]) * dirMult
-            case 'price': 
-            case 'stock': return (toy1[field] - toy2[field]) * dirMult
-        }
-    })
+    const sortOrder = isAscending ? 1 : -1
+    return { [field]: sortOrder }
 }
 
 function getPage(toys, pageInfo) {
     if (! pageInfo) return { toys, pageNum: 1, lastPageNum: 1 }
-    const toysPerPage = pageInfo.toysPerPage
-    const lastPageNum = Math.ceil(toys.length / toysPerPage) || 1
-    const pageNum = Math.min(Math.max(pageInfo.pageNum, 1), lastPageNum)
-    const startIdx = (pageNum - 1) * toysPerPage
-    const endIdx = startIdx + toysPerPage
-    const toyPage = {
-        resultsNum: toys.length,
-        toys: toys.slice(startIdx, endIdx),
-        pageNum,
-        lastPageNum,
+    if (! pageInfo.pageNum || ! pageInfo.toysPerPage) {
+        var toyPage = {
+            resultsNum: toys.length,
+            toys,
+            pageNum: 1,
+            lastPageNum: 1,
+        }
+    } else {
+        const toysPerPage = pageInfo.toysPerPage
+        const lastPageNum = Math.ceil(toys.length / toysPerPage) || 1
+        const pageNum = Math.min(Math.max(pageInfo.pageNum, 1), lastPageNum)
+        const startIdx = (pageNum - 1) * toysPerPage
+        const endIdx = startIdx + toysPerPage
+        var toyPage = {
+            resultsNum: toys.length,
+            toys: toys.slice(startIdx, endIdx),
+            pageNum,
+            lastPageNum,
+        }
     }
     return toyPage
 }
 
-function get(toyId) {
-    return query().then(({ toys }) => {
-            const toy = toys.find(toy => toy._id === toyId)
-            if (! toy) return Promise.reject('No such toy')
-            return toy
-        })
+async function get(toyId) {
+    try {
+        const toyCollection = await dbService.getCollection('toy')
+        const toy = await toyCollection.findOne({ _id: new ObjectId(toyId) })
+        return toy
+    } catch (err) {
+        loggerService.error(`Cannot get toy with id='${toyId}': `, err)
+        throw err
+    }
 }
 
-function save(toy) {
-    return query().then(({ toys }) => {
-            if (toy._id) {
-                const toyIdx = toys.findIndex(_toy => _toy._id === toy._id)
-                if (toyIdx < 0) return Promise.reject('No such toy')
-                const toyToEdit = toys[toyIdx]
-                if (toy.stockDiff) {
-                    toyToEdit.stock += toy.stockDiff
-                }
-                else for (let [key, value] of Object.entries(toy)) {
-                    if (value || value === 0) toyToEdit[key] = value
-                }
-                toy = toyToEdit
-            } else {
-                const newToy = _getNewToy()
-                for (let [key, value] of Object.entries(toy)) {
-                    if (value) newToy[key] = value
-                }
-                newToy._id = utilService.makeId()
-                toys.unshift(newToy)
-                toy = newToy
-            }
-            return _saveToys(toys).then(() => toy)
-        })
+async function save(toy) {
+    try {
+        var toyCollection = await dbService.getCollection('toy')
+    } catch (err) {
+        loggerService.error('Cannot get toy collection: ', err)
+        throw err
+    }
+    if (toy._id) {
+        try {
+            var toyToEdit = await get(toy._id)
+        } catch (err) {
+            loggerService.error('Cannot update toy: ', err)
+            throw err
+        }
+        if (! toyToEdit) return Promise.reject('No such toy')
+        if (toy.stockDiff) {
+            toyToEdit.stock += toy.stockDiff
+        }
+        else for (let [key, value] of Object.entries(toy)) {
+            if (value || value === 0) toyToEdit[key] = value
+        }
+        delete toyToEdit._id
+        try {
+            var res = await toyCollection.updateOne({ _id: new ObjectId(toy._id) }, { $set: toyToEdit })
+            if (! res.acknowledged) throw new Error('Did not acknowledge')
+        } catch (err) {
+            loggerService.error('Cannot update toy: ', err)
+            throw err
+        }
+        toyToEdit._id = toy._id
+        return toyToEdit
+    } else {
+        const newToy = _getNewToy()
+        for (let [key, value] of Object.entries(toy)) {
+            if (value) newToy[key] = value
+        }
+        toy = newToy
+        try {
+            var res = await toyCollection.insertOne(toy)
+            if (! res.acknowledged) throw new Error('Did not acknowledge')
+        } catch (err) {
+            loggerService.error('Cannot insert toy: ', err)
+            throw err
+        }
+        toy._id = res.insertedId
+        return toy
+    }
 }
 
-function remove(toyId) {
-    return query().then(({ toys }) => {
-            const toyIdx = toys.findIndex(toy => toy._id === toyId)
-            if (toyIdx < 0) return Promise.reject('No such toy')
-            const toy = toys[toyIdx]
-            toys.splice(toyIdx, 1)
-            return _saveToys(toys).then(() => toy)
-        })
+async function remove(toyId) {
+    try {
+        const toyCollection = await dbService.getCollection('toy')
+        var toy = await toyCollection.findOne({ _id: new ObjectId(toyId) })
+        if (! toy) throw new Error('No such toy')
+        const res = await toyCollection.deleteOne({ _id: new ObjectId(toyId) })
+        if (! res.deletedCount) throw new Error('Did not delete')
+    } catch (err) {
+        loggerService.error(`Cannot remove toy with id='${toyId}: '`, err)
+        throw err
+    }
+    return toy
 }
 
-function getAllLabels() {
-    return query().then(({ toys }) => {
+async function getAllLabels() {
+    try {
+        var toys = await query()
         return Array.from(new Set(toys.map(toy => toy.labels).flat())).sort((l1, l2) => l1.localeCompare(l2))
-    })
+    } catch (err) {
+        loggerService.error('Cannot get all albels: ', err)
+    }
 }
 
 function _getNewToy() {
--- default/toys.js
+++ default/toys.js
@@ -8,7 +8,6 @@ export function getDefaultToys() {
 
 const defaultToys = [
     {
-        _id: utilService.makeId(),
         stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Snuggle Buddy Bear",
         description: "A charming plush teddy bear, perfect for cuddles, sitting on a gentle pastel-colored background.",
@@ -16,7 +15,6 @@ const defaultToys = [
         labels: ["plush", "teddy bear", "soft", "cuddly"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Hug-A-Bear",
         description: "An adorable, huggable teddy bear with a unique pastel backdrop, ideal for cozy moments.",
@@ -24,7 +22,6 @@ const defaultToys = [
         labels: ["plush", "teddy", "comfort", "pastel"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "BrightMind Puzzle",
         description: "A vibrant wooden puzzle featuring primary colors, designed to be both fun and educational for young minds.",
@@ -32,7 +29,6 @@ const defaultToys = [
         labels: ["puzzle", "wooden", "educational", "colorful"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "ColorQuest Puzzle",
         description: "An eye-catching, colorful wooden puzzle that promises hours of educational play with its bright primary hues.",
@@ -40,7 +36,6 @@ const defaultToys = [
         labels: ["puzzle", "bright", "learning", "wooden"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Adventure Hero Figure",
         description: "An action figure in a dynamic pose, set against an adventurous backdrop, inspiring heroic play.",
@@ -48,7 +43,6 @@ const defaultToys = [
         labels: ["action figure", "superhero", "dynamic", "adventure"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Epic Warrior Toy",
         description: "This superhero action figure, in an exciting pose, comes with an adventurous scene, sparking imaginative play.",
@@ -56,7 +50,6 @@ const defaultToys = [
         labels: ["action figure", "hero", "pose", "imagination"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Family Game Night Board",
         description: "A classic board game setup that brings the family together with its engaging pieces and game board.",
@@ -64,7 +57,6 @@ const defaultToys = [
         labels: ["board game", "family", "classic", "fun"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Together Time Board Game",
         description: "Experience the joy of family time with this classic board game, featuring a fun and interactive setup.",
@@ -72,7 +64,6 @@ const defaultToys = [
         labels: ["game", "family fun", "interactive", "board"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Zoomster RC Car",
         description: "A sleek, remote-controlled car that captures the thrilling experience of RC vehicles in action.",
@@ -80,7 +71,6 @@ const defaultToys = [
         labels: ["remote-controlled", "car", "excitement", "action"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "SpeedRacer Remote Car",
         description: "Experience high-speed fun with this remote-controlled car, showcasing its dynamic capabilities.",
@@ -88,7 +78,6 @@ const defaultToys = [
         labels: ["RC car", "sleek", "fun", "speed"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Dreamy Dollhouse",
         description: "A beautifully detailed dollhouse, complete with dolls, offering a world of charm and imaginative play.",
@@ -96,7 +85,6 @@ const defaultToys = [
         labels: ["dollhouse", "dolls", "detail", "imagination"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Fantasy Villa",
         description: "This exquisite dollhouse set includes dolls and intricate details, igniting the joy of imaginative storytelling.",
@@ -104,7 +92,6 @@ const defaultToys = [
         labels: ["dollhouse", "fantasy", "storytelling", "detailed"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Melody Makers",
         description: "A set of colorful, child-friendly musical instruments including a charming small guitar and a xylophone, perfect for young music enthusiasts.",
@@ -112,7 +99,6 @@ const defaultToys = [
         labels: ["musical instruments", "colorful", "children", "fun"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Little Virtuoso Set",
         description: "This engaging set features kid-friendly musical instruments like a playful guitar and a vibrant xylophone, inspiring musical creativity.",
@@ -120,7 +106,6 @@ const defaultToys = [
         labels: ["music", "kid-friendly", "guitar", "xylophone"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Backyard Adventure Set",
         description: "A collection of outdoor toys including a delightful swing set and a fun sandbox, perfect for capturing the essence of outdoor play.",
@@ -128,7 +113,6 @@ const defaultToys = [
         labels: ["outdoor toys", "swing set", "sandbox", "play"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Fun in the Sun Kit",
         description: "This set includes joyful outdoor toys like a sturdy swing set and a creative sandbox, bringing endless hours of outdoor fun.",
@@ -136,7 +120,6 @@ const defaultToys = [
         labels: ["outdoor", "joyful play", "swing", "sandbox"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Junior Explorer Science Kit",
         description: "An educational and fun science kit for young explorers, featuring interactive experiments that spark curiosity and learning.",
@@ -144,7 +127,6 @@ const defaultToys = [
         labels: ["science kit", "educational", "explorers", "interactive"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Discovery Lab Set",
         description: "This engaging science kit offers young explorers a hands-on experience with exciting and interactive scientific experiments.",
@@ -152,7 +134,6 @@ const defaultToys = [
         labels: ["science", "fun", "educational", "hands-on"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Imaginary Kingdom Costumes",
         description: "A collection of fantasy play costumes for dress-up, including princess, knight, and astronaut outfits, perfect for role-playing games.",
@@ -160,7 +141,6 @@ const defaultToys = [
         labels: ["costumes", "dress-up", "fantasy", "role-playing"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Adventure Dress-Up Set",
         description: "Featuring enchanting costumes like princesses, knights, and astronauts, this set invites children into a world of imaginative role-play.",
@@ -168,7 +148,6 @@ const defaultToys = [
         labels: ["role-play", "costumes", "fantasy", "kids"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Creative Constructors Set",
         description: "Colorful building blocks and LEGO sets that foster creativity and construction play, designed to captivate and engage young minds.",
@@ -176,7 +155,6 @@ const defaultToys = [
         labels: ["building blocks", "LEGO", "creative", "children"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Build & Play Blocks",
         description: "An alluring set of colorful building blocks and LEGO, perfect for encouraging creativity and constructive play in children.",
@@ -184,7 +162,6 @@ const defaultToys = [
         labels: ["LEGO sets", "building", "colorful", "play"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Techie Tot Toy",
         description: "An interactive electronic learning toy that combines fun with education, showcasing the best of technology-based play.",
@@ -192,7 +169,6 @@ const defaultToys = [
         labels: ["electronic", "learning", "interactive", "educational"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Smart Play Gadget",
         description: "This electronic learning toy engages and educates, blending fun with technology to offer a modern play experience.",
@@ -200,7 +176,6 @@ const defaultToys = [
         labels: ["interactive toy", "electronic", "fun", "smart"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Classic Spinner Yo-Yo",
         description: "A single, brightly colored yo-yo with an intricate pattern, embodying the timeless appeal and fun of this classic toy.",
@@ -208,7 +183,6 @@ const defaultToys = [
         labels: ["yo-yo", "colorful", "classic", "intricate pattern"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Retro Magic Yo-Yo",
         description: "A single, brightly colored yo-yo with an elaborate pattern, harking back to the classic charm of this beloved toy.",
@@ -216,7 +190,6 @@ const defaultToys = [
         labels: ["yo-yo", "bright", "classic", "intricate"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Heritage Wooden Train",
         description: "A beautifully crafted wooden train toy, exemplifying classic craftsmanship and a timeless design ideal for collectors.",
@@ -224,7 +197,6 @@ const defaultToys = [
         labels: ["wooden toy", "train", "craftsmanship", "classic"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Timeless Locomotive",
         description: "This exquisitely crafted wooden train represents the pinnacle of traditional toy making, showcasing elegance and history.",
@@ -232,7 +204,6 @@ const defaultToys = [
         labels: ["wooden", "train", "detailed", "timeless"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Splashy the Rubber Duck",
         description: "A vibrant, colorful rubber duck designed to bring joy and fun to bath time, with a playful and engaging look.",
@@ -240,7 +211,6 @@ const defaultToys = [
         labels: ["rubber duck", "colorful", "bath toy", "playful"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Rainbow Duckie",
         description: "This cheerful, brightly colored rubber duck adds a touch of fun to any bath, captivating with its vibrant hues.",
@@ -248,7 +218,6 @@ const defaultToys = [
         labels: ["rubber duck", "vibrant", "fun", "bath time"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Sky High Model Airplane",
         description: "A detailed model airplane showcasing meticulous design and lifelike realism, perfect for aviation enthusiasts and collectors.",
@@ -256,7 +225,6 @@ const defaultToys = [
         labels: ["model airplane", "detailed", "realism", "collectors"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Aviator's Dream",
         description: "An intricately designed model airplane that captures the essence of aviation with its realism and attention to detail.",
@@ -264,7 +232,6 @@ const defaultToys = [
         labels: ["airplane model", "intricate", "realistic", "enthusiasts"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Collector's Edition Plane",
         description: "A single, expertly detailed model airplane, offering an unmatched level of realism, ideal for discerning collectors.",
@@ -272,7 +239,6 @@ const defaultToys = [
         labels: ["model airplane", "collector", "detailed", "realistic"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "FuturoBot Toy",
         description: "A brightly colored toy robot with a futuristic design, blending modern elements with playful charm.",
@@ -280,7 +246,6 @@ const defaultToys = [
         labels: ["robot", "colorful", "futuristic", "playful"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "RoboPlaymate",
         description: "This toy robot, adorned in vivid colors, showcases a modern, futuristic design, sparking imagination and play.",
@@ -288,7 +253,6 @@ const defaultToys = [
         labels: ["toy robot", "bright", "modern", "interactive"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Enchanted Fairy Doll",
         description: "A magical fairy doll with delicate wings and enchanting details, embodying the whimsical world of fantasy toys.",
@@ -296,7 +260,6 @@ const defaultToys = [
         labels: ["fairy doll", "magical", "wings", "fantasy"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Whimsy Winged Fairy",
         description: "This fairy doll captivates with its delicate wings and detailed design, bringing the magic of fairytales to life.",
@@ -304,7 +267,6 @@ const defaultToys = [
         labels: ["fairy", "delicate", "enchanted", "whimsical"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Sky Dancer Kite",
         description: "A single, colorful kite with a vibrant design, embodying the joy and freedom of outdoor play as it soars in the sky.",
@@ -312,7 +274,6 @@ const defaultToys = [
         labels: ["kite", "colorful", "outdoor", "joy"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Rainbow Flyer",
         description: "This eye-catching kite features a dynamic, colorful design, perfect for sunny days and outdoor adventures.",
@@ -320,7 +281,6 @@ const defaultToys = [
         labels: ["colorful kite", "vibrant", "outdoor fun", "soaring"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Mini Racer Pro",
         description: "A miniature toy car boasting realistic details and a sleek, modern design, capturing the thrill of miniature vehicle collectibles.",
@@ -328,7 +288,6 @@ const defaultToys = [
         labels: ["miniature car", "realistic", "sleek", "vehicle"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Speedster Mini Car",
         description: "This single, miniature toy car features intricate details and a streamlined design, perfect for enthusiasts of small-scale vehicles.",
@@ -336,7 +295,6 @@ const defaultToys = [
         labels: ["toy car", "miniature", "detailed", "collectible"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Mystic Unicorn Plush",
         description: "A whimsical unicorn plush toy, adorned with vibrant colors and soft textures, perfect for cuddles and imaginative play.",
@@ -344,7 +302,6 @@ const defaultToys = [
         labels: ["unicorn", "plush", "vibrant", "soft"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Rainbow Unicorn Toy",
         description: "This enchanting unicorn plush toy features a rainbow of colors and a delightfully soft texture, ideal for imaginative adventures.",
@@ -352,7 +309,6 @@ const defaultToys = [
         labels: ["unicorn plush", "whimsical", "colorful", "cuddly"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Brainy Cube Puzzle",
         description: "An intricate puzzle cube that presents a colorful and challenging brain teaser for puzzle enthusiasts of all ages.",
@@ -360,7 +316,6 @@ const defaultToys = [
         labels: ["puzzle cube", "intricate", "brain teaser", "colorful"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Color Twist Cube",
         description: "This single, complex puzzle cube offers a vibrant and engaging challenge, perfect for sharpening the mind.",
@@ -368,7 +323,6 @@ const defaultToys = [
         labels: ["puzzle", "colorful", "challenging", "mind game"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Panda Snuggle Pillow",
         description: "A soft and fluffy animal-shaped pillow designed as a cute panda, perfect for comfortable cuddles and decor.",
@@ -376,7 +330,6 @@ const defaultToys = [
         labels: ["pillow", "animal-shaped", "panda", "fluffy"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Cuddly Panda Pal",
         description: "This adorable, fluffy pillow shaped like a friendly panda offers a cozy and cute addition to any room.",
@@ -384,7 +337,6 @@ const defaultToys = [
         labels: ["panda pillow", "soft", "cuddly", "animal design"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Electro Train Express",
         description: "A single, electronic toy train with vibrant colors and realistic details, offering a modern twist on the classic train toy.",
@@ -392,7 +344,6 @@ const defaultToys = [
         labels: ["electronic train", "realistic", "vibrant", "modern"]
     },
     {
-		_id: utilService.makeId(),
 		stock: Math.max(utilService.getRandInt(16, -4), 0),
         name: "Vivid Locomotive",
         description: "This electronic toy train combines realistic detailing with bright, eye-catching colors, reinventing the classic train toy experience.",
